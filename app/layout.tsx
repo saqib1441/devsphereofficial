7b7f9a0dc679753a246d4a48d9756a23fd4f8df5
saqi1441 <msaqibalilolai@gmail.com>
2025-03-11 17:02:31 +0500

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,15 @@ import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 
 const poppins = Poppins({
-  variable: "--font-poppins",
   subsets: ["latin"],
   weight: "400",
+  display: "swap",
 });
 
 const nunito = Nunito_Sans({
-  variable: "--font-nunito",
   subsets: ["latin"],
   weight: "900",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,17 +24,36 @@ export const metadata: Metadata = {
     "At Dev Sphere, we provide top-notch IT services, including web development, app development, digital marketing, SEO, WordPress, UI/UX design, and graphic design. Our expert team is dedicated to delivering innovative and customized solutions that help businesses thrive in the digital world. Partner with us for cutting-edge technology and exceptional results!",
   icons: {
     icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Dev Sphere - An IT Software Company",
+    description:
+      "We provide expert IT services, including web development, SEO, digital marketing, and UI/UX design.",
+    url: "https://yourwebsite.com",
+    siteName: "Dev Sphere",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Dev Sphere Banner",
+      },
+    ],
+    type: "website",
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <head>
+        {/* Meta Tags */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#ffffff" />
+        {/* Favicon Links */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -52,14 +71,14 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="icon" sizes="any" href="/favicon.ico" />
         <meta
           name="google-site-verification"
           content="7cT9PWonrQ2_sew-UQkEqur8FYO4HK8gUpLy3z9ejCo"
         />
       </head>
-      <body className={`${nunito.variable} ${poppins.className} antialiased`}>
+      <body className={`${nunito.className} ${poppins.className} antialiased`}>
         <ThemeProvider>
           <Navbar />
           {children}
