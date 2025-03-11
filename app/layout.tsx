5f75fd592855a3b732ed430366c0167f61736e3b
saqi1441 <msaqibalilolai@gmail.com>
2025-03-11 11:27:21 +0500

    adding google console verification html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,10 @@ export default function RootLayout({
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="google-site-verification"
+          content="7cT9PWonrQ2_sew-UQkEqur8FYO4HK8gUpLy3z9ejCo"
+        />
       </head>
       <body className={`${nunito.variable} ${poppins.className} antialiased`}>
         <ThemeProvider>
